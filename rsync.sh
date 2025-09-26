1daa4cc9555cb6abbd27da4f43f8ac5ca34aaf59
Giovanni Gaudino <gaudino@na.infn.it>
2025-09-26 11:14:22 +0200

    add rsync

---
+++ rsync.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Script per eseguire rsync ogni 15 minuti
+while true; do
+    echo "$(date): Avvio rsync..."
+    rsync -avh --progress fcc@128.141.41.208:/data/runs/*mid.gz /eos/experiment/drdcalo/maxicc/TBCERN_24Sept2025_vx2730/
+    echo "$(date): Rsync completato. Attendo 15 minuti..."
+    sleep 900  # 900 secondi = 15 minuti
+done
